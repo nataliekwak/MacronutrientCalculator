fc5f9e793955b20709567743046ec7a1ae222606
nataliekwak <natalie.ann.kwak@gmail.com>
2022-12-02 19:59:37 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,9 @@ int main ()
     cout << "update";
     cout << "update2";
 
-<<<<<<< HEAD
-
-
 
     //Nikhil
     // ------------------------------------------------------------------
-=======
-    cout << "Tommy"
->>>>>>> 14bbd8e47bb8a0bbf6cd52a06f5a592fc045fdbb
 }
 
 // Natalie's section
