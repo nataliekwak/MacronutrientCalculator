45f89e512bb9ed4bf25ade646c844be5ac3c855f
TommyMac15 <Tmcmullen1@ufl.edu>
2022-12-02 19:57:03 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,6 @@ int main ()
     cout << "update";
     cout << "update2";
 
-<<<<<<< HEAD
-
-
-
-    //Nikhil
-    // ------------------------------------------------------------------
-=======
-    cout << "Tommy"
->>>>>>> 14bbd8e47bb8a0bbf6cd52a06f5a592fc045fdbb
 }
 
 // Natalie's section
@@ -25,3 +16,6 @@ void print()
 {
     cout << "printing";
 }
+
+
+// Merge sort
