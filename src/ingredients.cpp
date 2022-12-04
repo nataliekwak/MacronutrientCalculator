76c2861f9018e2311b3b07f2883183b254b6238f
TommyMac15 <Tmcmullen1@ufl.edu>
2022-12-04 11:55:29 -0500

    Edit
    Additional operators and values

--- src/ingredients.cpp
+++ src/ingredients.cpp
@@ -6,16 +6,20 @@ struct Ingredient
 {
     string genericName; // Index 0 of the csv
     string specificName; // Index 1
+    float carbohydrate; // Index 6
+    float cholesterol; // Index 7
     float protein; // Index 13 
     float sugar; // Index 17 
 
     // Add any other values we might have to store about each ingredient
 
     // Constructor
-    Ingredient (string shortName, string longName, float p, float s)
+    Ingredient (string shortName, string longName, float carb, float chol; float p, float s)
     {
         genericName = shortName;
         specificName = longName;
+        carbohydrate = carb;
+        cholesterol = chol;
         protein = p;
         sugar = s;
     }
@@ -25,6 +29,30 @@ struct Ingredient
     {
         return genericName == i.genericName && specificName == i.specificName;
     }
+    string getGeneric(Ingredient *node){
+        return node->genericName;
+    }
+
+    string getSpecific(Ingredient *node){
+        return node->specificName;
+    }
+
+    float getCarb(Ingredient *node){
+        return node->carbohydrate;
+    }
+
+    float getChol(Ingredient *node){
+        return node->cholesterol;
+    }
+
+    float getProtein(Ingredient *node){
+        return node->protein;
+    }
+
+    float getSugar(Ingredient *node){
+        return node->sugar;
+    }
+
 };
 
 // Class for our hash function
