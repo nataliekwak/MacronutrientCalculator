1df0f4ae7ae0c8a32656b43e4aa1d0e1cbec219b
TommyMac15 <Tmcmullen1@ufl.edu>
2022-12-04 13:19:21 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "ingredients.cpp"
-
+#include <unordered_map>
 #include <bits/stdc++.h>
+#include <vector>
 using namespace std;
 
 // Helper function to read the passed in csv file and return a populated hashmap 
@@ -13,13 +14,37 @@ unordered_map<Ingredient, int, HashFunction> populateHashMap (string file)
     return hashMap;
 }
 
+//helper function to sort with merge sort
+void mergeSort(unordered_map<Ingredient, int, HashFunction> *hashMap, string sortBy){
+    
+}
+
+
+
+
 int main ()
 {
     // Make hashmap by calling populateHashMap("ingredients.csv")
+    unordered_map<Ingredient, int, HashFunction> hashMap;
+    hashMap = populateHashMap("Ingredients.csv");
+
 
     // Output
 
     // Take user input
+    while (true){
+        int choice;
+        cout << "0. Exit\n" << "1. Print ingredients by protein\n" << "3. Check specifc ingredient\n";
+        cin >> choice;
+
+        if (choice == 0){
+            return;
+        }
+        if (choice == 1){
+
+        }
+
+    }
     
     return 0;
 }
